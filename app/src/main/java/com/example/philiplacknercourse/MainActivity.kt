fb996b769e619bc56c161dc1a3da6ef7cdce4bf6
Surya Ashish <ashishsurya126@gmail.com>
2022-05-23 00:32:04 +0530

    Lesson completed

--- app/src/main/java/com/example/philiplacknercourse/MainActivity.kt
+++ app/src/main/java/com/example/philiplacknercourse/MainActivity.kt
@@ -10,16 +10,17 @@ import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import kotlinx.android.synthetic.main.activity_main.*
 
 /*
-* LESSON ON HOW TO CREATE ALERT DIALOGS.
-* TYPE 1 : A DIALOG WITH YES/NO RESPONSE.
-* TYPE 2 : A DIALOG WERE USER SHOULD SELECT ONE OPTION FROM MANY AND ACCEPT THE DIALOG AND ALSO CAN DENY THE DIALOG WITHOUT CHOOSING.
-* TYPE 3 : SAME AS TYPE2 BUT CAN SELECT MANY OPTIONS
+* LESSON ON HOW TO CREATE SPINNERS(dropdowns with more options).
+*
 *
 *
 * */
@@ -29,83 +30,32 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val addContactDialog = AlertDialog.Builder(this)
-            .setTitle("Add Contact")
-            .setMessage("Do you want to add Mr.Surya to your contacts list???")
-            .setIcon(R.drawable.profile)
-            .setPositiveButton("Yes") { _, _ ->
-                Toast.makeText(this, "You added Mr.Surya to your contact list", Toast.LENGTH_SHORT)
-                    .show()
-            }
-            .setNegativeButton("No") { _, _ ->
+        val customList = listOf("First", "Second", "Third", "Fourth")
+        val adapter = ArrayAdapter<String>(
+            this,
+            com.google.android.material.R.layout.support_simple_spinner_dropdown_item,
+            customList
+        )
+        spMonths.adapter = adapter
+        spMonths.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                adapterView: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
                 Toast.makeText(
-                    this,
-                    "You didnt add Mr.Surya to your contact list",
+                    this@MainActivity,
+                    "You selected ${adapterView?.getItemAtPosition(position).toString()}",
                     Toast.LENGTH_SHORT
                 ).show()
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
 
             }
-            .create()
 
-
-        btnD1.setOnClickListener {
-            addContactDialog.show()
         }
-
-
-        val options = arrayOf("First Item", "Second Item", "Third Item");
-        val singleChoiceDialog = AlertDialog.Builder(this)
-            .setTitle("Choose one of these options.")
-            .setSingleChoiceItems(options, 0) { _, i ->
-                Toast.makeText(this, "You clicked on ${options[i]}", Toast.LENGTH_SHORT).show()
-            }
-            .setPositiveButton("Accept") { _, _ ->
-                Toast.makeText(this, "You accepted the single choice dialog", Toast.LENGTH_SHORT)
-                    .show()
-            }
-            .setNegativeButton("No") { _, _ ->
-                Toast.makeText(
-                    this,
-                    "You didn't accept the singe choice dialog",
-                    Toast.LENGTH_SHORT
-                ).show()
-
-            }
-            .create()
-
-        btnD2.setOnClickListener {
-            singleChoiceDialog.show()
-        }
-
-
-        val multiChoiceDialog = AlertDialog.Builder(this)
-            .setTitle("Choose one of these options.")
-            .setMultiChoiceItems(options, booleanArrayOf(false, false, false)) { _, i, isChecked ->
-                if (isChecked)
-                    Toast.makeText(this, "You checked ${options[i]}", Toast.LENGTH_SHORT).show()
-                else
-                    Toast.makeText(this, "You unchecked ${options[i]}", Toast.LENGTH_SHORT).show()
-
-            }
-            .setPositiveButton("Accept") { _, _ ->
-                Toast.makeText(this, "You accepted the multi choice dialog", Toast.LENGTH_SHORT)
-                    .show()
-            }
-            .setNegativeButton("No") { _, _ ->
-                Toast.makeText(
-                    this,
-                    "You didn't accept the multi choice dialog",
-                    Toast.LENGTH_SHORT
-                ).show()
-
-            }
-            .create()
-
-        btnD3.setOnClickListener {
-            multiChoiceDialog.show()
-        }
-
-
     }
 
 
